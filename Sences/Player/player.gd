8b086a9f0462b13f948d049c0fa094faf75aa568
DeerLuuu <17359672787@163.com>
2024-03-04 19:04:57 +0800

    角色的待机、移动、爬墙、跳越的动画创建，角色的代码（包含移动、跳跃、蹬墙跳、爬墙等），注意：蹬墙跳有Bug，具体为当玩家处于爬墙状态时按住面向墙壁那一面进行蹬墙跳最后执行的是向上爬

---
+++ Sences/Player/player.gd
@@ -0,0 +1,96 @@
+extends CharacterBody2D
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
+
+
+@export var speed := 250.0
+@export var jump_speed := -300.0
+@export var wall_jump_speed := Vector2(500, -350.0)
+
+
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var dir := 0.0
+var acceleration : float
+var is_wall_jump := false
+var air_time := 0.0
+
+#region 虚方法
+func _ready() -> void:
+	air_time = 0
+	acceleration = speed / .5
+	animation_player.play("idle")
+
+
+func _process(delta: float) -> void:
+	if is_on_wall_only() and not is_wall_jump:
+		dir = -1 if sprite_2d.flip_h else 1
+	else :
+		dir = Input.get_axis("action_left", "action_right")
+	is_flip()
+	anim_player()
+
+
+func _physics_process(delta: float) -> void:
+	# 重力的变化
+	if is_wall_jump:
+		acceleration = speed / .2
+	elif not is_on_floor() and not is_on_wall_only():
+		acceleration = speed / .2
+		velocity.y += gravity * delta
+	elif is_on_wall_only():
+		velocity.y = 0
+		velocity.y += gravity * delta * .6
+	
+	# 跳越的情况设置
+	if Input.is_action_just_pressed("action_jump"):
+		if is_on_floor():
+			air_time = 0
+			velocity.y = jump_speed
+		elif is_on_wall_only():
+			is_wall_jump = true
+			velocity = wall_jump_speed
+			velocity.x *= get_wall_normal().x
+	
+	# 再空中的时间
+	if air_time > 0.1:
+		air_time = 0
+		is_wall_jump = false
+		
+	
+	# 角色的移动
+	velocity.x = move_toward(velocity.x, dir * speed, acceleration * delta) if not is_wall_jump else dir * speed
+	
+	# 启用 velocity 移动
+	move_and_slide()
+	
+	# 如果使用蹬墙跳再空中的不受重力时间
+	if is_wall_jump:
+		air_time += delta
+#endregion
+
+#region 角色翻转的代码
+func is_flip() -> void:
+	# 判断方向不为0时，根据 dir 的值修改 sprite 的 flip_h 的值
+	if not is_zero_approx(dir):
+		sprite_2d.flip_h = dir < 0
+	
+	# 修改碰撞体的方向，根据 sprite 的方向是否翻转来判断是否翻转 collision
+	collision_polygon_2d.scale.x = -1 if sprite_2d.flip_h else 1
+#endregion
+
+#region 动画的切换
+func anim_player() -> void:
+	if is_on_floor():
+		if is_zero_approx(dir) and is_zero_approx(velocity.x):
+			animation_player.play("idle")
+		else :
+			animation_player.play("run")
+	else :
+		if is_on_wall_only():
+			animation_player.play("wall_slide")
+		else :
+			animation_player.play("jump")
+#endregion
